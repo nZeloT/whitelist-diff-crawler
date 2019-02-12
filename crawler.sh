c88becc0f1c32eab8a722113ee52e9d358e14561
nzelot <leontsteiner@gmail.com>
2019-02-12 08:24:14 +0100

    reduced

--- crawler.sh
+++ crawler.sh
@@ -8,7 +8,6 @@ report="report_$(date +'%d-%m-%Y').txt"
 rm $report
 touch "$report"
 count=0
-found="NODIFF"
 while read line; do
   echo "Curling ... $line"
   url=$(echo $line | cut -d'|' -f 2)
@@ -21,30 +20,11 @@ while read line; do
   then
 	echo "Found difference!"
 	echo "$line" >> $report
-	found="DIFF"
   fi
 done <diffcheck.txt
 echo "Done diffing. Deleting old, moving new"
 rm -r old
 echo "Finished. Find result in $report"
-notepad $report
-echo "Preparing Mail ..."
-echo $found | grep "NODIFF$"
-if [[ $? -eq 0 ]]
-then
-  echo "No difference mail ..."
-  mail="No Updates found."
-else
-  echo "Diff mail ..."
-  mail="Updates found on:<br><ul>"
-  while read line; do
-    name=$(echo $line | cut -d'|' -f 1)
-    url=$(echo $line | cut -d'|' -f 2)
-    mail="$mail<li><a href='$url'>$name</a></li>"
-  done <$report
-  mail="$mail</ul>"
-  echo $mail
-fi
-echo "Sending mail ..."
-java -cp ".;./javax.mail.jar;./javax.activation.jar" MessageSend "[$found] $(date +'%d-%m-%Y')" "$mail"
 mv new old
+notepad $report
+
